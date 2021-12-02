8dbe783356d02899122eac2f03b1dc6a8ad27398
sgudia <Sgudia939@gmail.com>
2021-12-02 15:56:50 +0000

    commit

--- src/test/features/smoke.feature
+++ src/test/features/smoke.feature
@@ -1,7 +1,6 @@
-
 @Smoke
 Feature:SmokeTest
-  Scenario: TC_01_User lauch amazon website successful
+  Scenario: TC_01_User launch amazon website successful
     Given I launch amazon url
     Then I verify if logo is displayed
     Then I verify text is displayed:"Hello, Sign in"
--- test-output/report.js
+++ test-output/report.js
@@ -2,7 +2,12 @@ $(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.
 formatter.feature({
   "name": "Home page",
   "description": "",
-  "keyword": "Feature"
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@Regression"
+    }
+  ]
 });
 formatter.background({
   "name": "I am background for amazon web application",
@@ -41,16 +46,6 @@ formatter.match({
 });
 formatter.result({
   "status": "passed"
-});
-formatter.scenario({
-  "name": "TC_01_Add to cart",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@cart"
-    }
-  ]
 });
 formatter.step({
   "name": "I search for item:\"lego75955\"",
@@ -93,7 +88,7 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "I verify product title from product description page:\"LEGO 75955 Super Mario Bowser\u0027s Airship Expansion Set, Collectible Buildable Game Toy for Kids with 3 Figures\"",
+  "name": "I verify product title from product description page:\"LEGO 75955 Harry Potter Hogwarts Express Train Toy, Wizarding World Fan Gift, Building Sets for Kids\"",
   "keyword": "Then "
 });
 formatter.match({
@@ -102,22 +97,25 @@ formatter.match({
 formatter.result({
   "status": "passed"
 });
+formatter.scenario({
+  "name": "TC_01_Add to cart",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Regression"
+    },
+    {
+      "name": "@Smoke"
+    }
+  ]
+});
 formatter.step({
   "name": "I click on add to basket btn",
   "keyword": "And "
 });
 formatter.match({
   "location": "ProductDescriptionPageStep.iClickOnAddToBasketBtn()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I verify quantity on basket icon is:\"1\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "TopActionBlackPageStep.iVerifyQuantityOnBasketIconIs(String)"
 });
 formatter.result({
   "status": "passed"
@@ -138,6 +136,194 @@ formatter.step({
 });
 formatter.match({
   "location": "ShoppingCartPageStep.iVerifyProductTitleFromCartPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "I am background for amazon web application",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I launch amazon url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HomePageStep.i_am_on_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I accept cookies",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "HomePageStep.acceptCookie()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify if logo is displayed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStep.isLogoDisplayed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I search for item:\"lego75955\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "HomePageStep.enterSearchCriteria(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify lego75955 is displayed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStep.iVerifyLegoIsDisplayed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on lego75955 product",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "HomePageStep.iClickOnLegoProduct()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify current page url is:\"https://www.amazon.co.uk/LEGO-75955\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStep.iverifyUrl(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify product title from product description page:\"LEGO 75955 Harry Potter Hogwarts Express Train Toy, Wizarding World Fan Gift, Building Sets for Kids\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ProductDescriptionPageStep.verifyTitle(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Tc_02_verify quantity from basket icon",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "I select quantity two",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProductDescriptionPageStep.iClickQuantityDropDown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on add to basket btn",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProductDescriptionPageStep.iClickOnAddToBasketBtn()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify quantity on basket icon is:\"2\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TopActionBlackPageStep.iVerifyQuantityOnBasketIconIs(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/features/smoke.feature");
+formatter.feature({
+  "name": "SmokeTest",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "TC_01_User launch amazon website successful",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I launch amazon url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HomePageStep.i_am_on_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify if logo is displayed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStep.isLogoDisplayed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify text is displayed:\"Hello, Sign in\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStep.iVerifyTextIsDisplayed(String)"
 });
 formatter.result({
   "status": "passed"
